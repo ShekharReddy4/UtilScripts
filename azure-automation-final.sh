80f647ab64c1906702ac0f6c55017efea58f1041
ShekharReddy <reddyshekhar619@gmail.com>
2021-07-09 02:05:31 +0530

    correction of a small mistake

--- azure-automation-final.sh
+++ azure-automation-final.sh
@@ -4,7 +4,7 @@
 az login 
 
 # Temporarily change the path to private dist
-export PATH=:$PATH
+#export PATH=:$PATH
 
 # Adding the CLI extension for spring-cloud 
 az extension add --name spring-cloud
@@ -20,7 +20,7 @@ az extension add --name spring-cloud
 
 echo "PLEASE ENTER A APPROPRIATE NUMBER FOR SELECTING THE Environment "
 echo "Please Enter 1 for e2e, 2 for dev, 3 for prod : "
-read $ENV
+read ENV
 
 # FIGURE OUT THE RESOURCE GROUPS
 if (( "$ENV" = 1)); then
@@ -39,11 +39,11 @@ ENV_RG_LIST=$(echo $RG_LIST | tr ' ' '\n' | grep $ENV_STRING)
 
 # GET THE SERVICE 
 echo "Please Enter 1 for Azure Spring-Cloud and 2 for App Service : "
-read $SVC
+read SVC
 
 # GET THE ACTION USER WANTS TO PERFORM
 echo "Please Enter 1 for Start, \n 2 for Stop, \n 3 for Restart : "
-read $ACT
+read ACT
 
 #-----------------------------------------------------------------------------------------------------------------------------------------#
 #-----------------------------------------------------------------------------------------------------------------------------------------#
